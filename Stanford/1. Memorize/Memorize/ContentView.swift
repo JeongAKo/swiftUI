77b53af21039247e3c320f66ceab411445a5032a
Daisy <dev.daisiee@gmail.com>
2020-11-10 18:57:17 +0900

    stanford lecture 추가

---
+++ Stanford/1. Memorize/Memorize/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 고정아 on 2020/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello There, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+ 
---
+++ Stanford/Docs/Lecture1.md
@@ -0,0 +1,9 @@
+# Lecture 1: Course Logistics and Introduction to SwiftUI
+
+ swiftUI → functional programmaing
+
+UIkit → OOP
+
+
+
+Pakage is SwiftUI
