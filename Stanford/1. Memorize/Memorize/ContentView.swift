4cf663bc5782424f862081a71ccc1f7bee7cf5dd
Daisy <dev.daisiee@gmail.com>
2020-11-12 18:52:19 +0900

    Add: Lecture 2: MVVM and the Swift Type System

--- Stanford/1. Memorize/Memorize/ContentView.swift
+++ Stanford/1. Memorize/Memorize/ContentView.swift
@@ -7,45 +7,47 @@
 
 import SwiftUI
 
+//View
 struct ContentView: View {
+  var viewModel: EmojiMemoryGame
   var body: some View {
     //ForEach(iteratablething //like Array, Dict, tuple,,,ets something can return object )
     HStack {
-      ForEach(0..<4) { index in
-        CardView(isfaceUp: true)
+      ForEach(viewModel.cards) { card in
+        CardView(card: card)
       }
     }
     
     .padding()
     .foregroundColor(Color.orange)
     .font(Font.largeTitle)
-    
+     
   }
 }
 
 
 struct CardView: View {
-  var isfaceUp: Bool
+  var card: MemoryGame<String>.Card
   
   var body: some View {
     ZStack {
-      if isfaceUp {
+      if card.isFaceUp {
         RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
         RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-        Text("🎃")
+        Text(card.content)
       } else {
         RoundedRectangle(cornerRadius: 10.0).fill()
       }
     }
   }
 }
-
+ 
 
 
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(viewModel: EmojiMemoryGame())
   }
 }
 
